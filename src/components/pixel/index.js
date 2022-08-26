36d384a04f244a98fbd0a16024cc14a922287199
Phung Pha <phungmac@Phungs-MacBook-Pro.local>
2022-08-26 01:24:55 -0500

    fixed pixel

--- src/components/pixel/index.js
+++ src/components/pixel/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Head  from 'next/head'
-
-import FACEBOOK_PIXEL_1 from './facebook/pixel-1'
+import FACEBOOK_PIXEL_1 from './Facebook/pixel-1'
 
 export default ({name}) => {
 
